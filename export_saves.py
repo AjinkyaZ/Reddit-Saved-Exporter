e2718b45790dbceaa4884fa6e068d05e8a3afb68
Ajinkya Zadbuke <ajinkya.zadbuke@gmail.com>
2021-07-31 12:18:37 -0400

    clean up a bit

--- export_saves.py
+++ export_saves.py
@@ -13,56 +13,62 @@ CLIENT_SECRET = ''
 reddit = praw.Reddit(client_id=CLIENT_ID,
                      client_secret=CLIENT_SECRET,
                      password=REDDIT_PASSWORD,
-                     user_agent='reddit saves exporter /u/'+REDDIT_USERNAME,
+                     user_agent='reddit saves exporter /u/' + REDDIT_USERNAME,
                      username=REDDIT_USERNAME)
 
 me = reddit.redditor(REDDIT_USERNAME)
 
 saved = {}
 start = time()
-for index, i in enumerate(me.saved()):
-    name = i.name
-    if i.name[:2]=='t1':
+for ix, item in enumerate(me.saved(limit=None)):
+    item_name = item.name
+    item_id = item.id
+    if item_name[:2] == 't1':
         type_saved = 'comment'
-        content = i.body.strip()
-        link = 'https://reddit.com'+i.permalink()
-    elif i.name[:2]=='t3':
+        content = item.body.strip()
+        link = 'https://reddit.com' + item.permalink
+        print(f"ID: {item_name} TYPE: COMMENT")
+    elif item.name[:2] == 't3':
         type_saved = 'post'
-        content = i.title.strip()
-        link = i.url
+        content = item.title.strip()
+        print(f"ID: {item_name} TYPE: POST TITLE: {item.title.strip()}")
+        link = item.url
     else:
-        continue 
+        continue
     sub = re.findall(r"/r/([^\s/]+)", link)
     try:
         sub = sub[0]
-    except IndexError, e:
+    except IndexError as e:
         sub = u'Image/External Link'
-    saved[index] = {'Name': name, 
-                    'Type':type_saved,
-                    'Content': content,
-                    'Url': link,
-                    'Subreddit': sub
-                   }
+    saved[ix] = {
+        'Name': item_name,
+        'Type': type_saved,
+        'Content': content,
+        'Url': link,
+        'Subreddit': sub
+    }
 
-print "SAVED:", len(saved), "links"
-print "TOOK :", time()-start, "seconds"
-print "AVG  :", (time()-start)/float(len(saved))
+print("SAVED:", len(saved), "links")
+print("TOOK :", time() - start, "seconds")
+print("AVG  :", (time() - start) / float(len(saved)))
 
 with open('./saved_links.json', 'w') as f:
     json.dump(saved, f)
 
 with codecs.open('./saved_links.md', 'w', encoding='utf-8') as f:
-    for i in saved:
-        subr = saved[i]['Subreddit']
-        new_content = re.sub(r'_{3,}', '', saved[i]['Content'])
+    for item in saved:
+        subr = saved[item]['Subreddit']
+        new_content = re.sub(r'_{3,}', '', saved[item]['Content'])
         if subr == u'Image/External Link':
-            f.write(str(i+1)+"  :\n")
+            f.write(str(item + 1) + "  :\n")
         else:
-            f.write(str(i+1)+"  :  [r/"+subr+']('+'https://reddit.com/'+subr+')\n')
-        if saved[i]['Type'] == 'post':
-            f.write("["+new_content+"]("+saved[i]['Url']+")")
+            f.write(
+                str(item + 1) + "  :  [r/" + subr + '](' +
+                'https://reddit.com/' + subr + ')\n')
+        if saved[item]['Type'] == 'post':
+            f.write("[" + new_content + "](" + saved[item]['Url'] + ")")
         else:
             f.write("\n")
-            f.write("[Permalink]("+saved[i]['Url']+")\n")
+            f.write("[Permalink](" + saved[item]['Url'] + ")\n")
             f.write(new_content)
         f.write("\n********************************************\n")
