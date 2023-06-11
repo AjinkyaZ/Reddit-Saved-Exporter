6de0810ff157c54dc29f8d931d45b4dee8957106
Ajinkya Zadbuke <ajinkya.zadbuke@gmail.com>
2023-06-11 11:38:02 -0400

    Update script

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# Reddit_Saved_Exporter
+# Reddit Saved Links Exporter
 Exports your Reddit saved links to Markdown and JSON  
 ---
-Client Auth data can be obtained by going to reddit.com/your_username,  
-selecting Preferences>Apps>Create another app.  
-Once done, click on edit below the app's icon to get the CLIENT_SECRET.  
-The CLIENT_ID is located right under the app's name.  
+Client auth data can be obtained by going to `reddit.com/<REDDIT_USERNAME>`,   
+selecting Preferences > Apps > Create another app.  
+Once done, click on edit below the app's icon to get the `CLIENT_SECRET`.  
+The `CLIENT_ID` is located right under the app's name.  
 
 Based on [github.com/export-saved-reddit](https://github.com/csu/export-saved-reddit), which is probably way more powerful/extensive.  
-Requires [Praw](https://github.com/praw-dev/praw). To install, just type ```pip install praw``` in your terminal.  
+Requires [Praw](https://github.com/praw-dev/praw). To install, just type ```pip install praw``` in your terminal.   
+Works as of June 2023.
--- export_saves.py
+++ export_saves.py
@@ -1,74 +1,112 @@
 import codecs
 import json
 import praw
-from pprint import pprint
 import re
 from time import time
+from datetime import datetime
+import sys
+import argparse
 
-REDDIT_USERNAME = ''
-REDDIT_PASSWORD = ''
-CLIENT_ID = ''
-CLIENT_SECRET = ''
 
-reddit = praw.Reddit(client_id=CLIENT_ID,
-                     client_secret=CLIENT_SECRET,
-                     password=REDDIT_PASSWORD,
-                     user_agent='reddit saves exporter /u/' + REDDIT_USERNAME,
-                     username=REDDIT_USERNAME)
+def parse_args(main_args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--username", help="", type=str, default="")
+    parser.add_argument("-p", "--password", help="", type=str, default="")
+    parser.add_argument("-i", "--client-id", help="", type=str, default="")
+    parser.add_argument("-s", "--client-secret", help="", type=str, default="")
+    parser.add_argument("-v", "--verbose", help="", type=bool, default=False)
+    args = parser.parse_args(main_args)
 
-me = reddit.redditor(REDDIT_USERNAME)
+    return args
 
-saved = {}
-start = time()
-for ix, item in enumerate(me.saved(limit=None)):
-    item_name = item.name
-    item_id = item.id
-    if item_name[:2] == 't1':
-        type_saved = 'comment'
-        content = item.body.strip()
-        link = 'https://reddit.com' + item.permalink
-        print(f"ID: {item_name} TYPE: COMMENT")
-    elif item.name[:2] == 't3':
-        type_saved = 'post'
-        content = item.title.strip()
-        print(f"ID: {item_name} TYPE: POST TITLE: {item.title.strip()}")
-        link = item.url
-    else:
-        continue
-    sub = re.findall(r"/r/([^\s/]+)", link)
-    try:
-        sub = sub[0]
-    except IndexError as e:
-        sub = u'Image/External Link'
-    saved[ix] = {
-        'Name': item_name,
-        'Type': type_saved,
-        'Content': content,
-        'Url': link,
-        'Subreddit': sub
-    }
 
-print("SAVED:", len(saved), "links")
-print("TOOK :", time() - start, "seconds")
-print("AVG  :", (time() - start) / float(len(saved)))
+def main():
+    args = parse_args(sys.argv[1:])
 
-with open('./saved_links.json', 'w') as f:
-    json.dump(saved, f)
+    reddit = praw.Reddit(client_id=args.client_id,
+                         client_secret=args.client_secret,
+                         username=args.username,
+                         password=args.password,
+                         user_agent=f"savedexporter:v0.1.0")
 
-with codecs.open('./saved_links.md', 'w', encoding='utf-8') as f:
-    for item in saved:
-        subr = saved[item]['Subreddit']
-        new_content = re.sub(r'_{3,}', '', saved[item]['Content'])
-        if subr == u'Image/External Link':
-            f.write(str(item + 1) + "  :\n")
+    TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+    me = reddit.user.me()
+    print(f"Fetching saved links for u/{me}")
+
+    verbose = args.verbose
+
+    saved = []
+    start = time()
+    for item_ix, item in enumerate(me.saved(limit=None)):
+        item_name = item.name
+        item_created = datetime.utcfromtimestamp(
+            item.created_utc).strftime(TIME_FORMAT)
+
+        item_title = "N/A"
+        if item_name[:2] == "t1":
+            type_saved = "comment"
+            content = item.body.strip()
+            # https://reddit.com/r/<SUBREDDIT>/comments/<POST_ID>/<POST_TITLE>/<COMMENT_ID>/"
+            post_id = item.permalink.split("/")[4]
+            post_item = list(reddit.info(fullnames=[f"t3_{post_id}"]))[0]
+            item_title = post_item.title.strip()
+            link = "https://reddit.com" + item.permalink
+        elif item.name[:2] == "t3":
+            type_saved = "post"
+            item_title = item.title.strip()
+            content = item.selftext.strip()
+            link = item.url
         else:
-            f.write(
-                str(item + 1) + "  :  [r/" + subr + '](' +
-                'https://reddit.com/' + subr + ')\n')
-        if saved[item]['Type'] == 'post':
-            f.write("[" + new_content + "](" + saved[item]['Url'] + ")")
-        else:
-            f.write("\n")
-            f.write("[Permalink](" + saved[item]['Url'] + ")\n")
-            f.write(new_content)
-        f.write("\n********************************************\n")
+            continue
+
+        sub = re.findall(r"/r/([^\s/]+)", link)
+        try:
+            subreddit = sub[0]
+        except IndexError as e:
+            subreddit = "Image/External Link"
+
+        author_name = "N/A"
+        if item.author is not None:
+            author_name = item.author.name
+        item_dict = {
+            "id": item.id,
+            "type": type_saved,
+            "title": item_title,
+            "content": content,
+            "url": link,
+            "subreddit": subreddit,
+            "created": item_created,
+            "author": author_name,
+            "score": item.score,
+            "awards": item.total_awards_received,
+        }
+        # pprint(item_dict)
+        saved.append(item_dict)
+
+    print("SAVED:", len(saved), "links")
+    print("TOOK :", time() - start, "seconds")
+    print("AVG  :", (time() - start) / float(len(saved)))
+
+    with open("./saved_links.json", "w") as f:
+        json.dump(saved, f)
+
+    with codecs.open("./saved_links.md", "w", encoding="utf-8") as f:
+        for item_ix, item in enumerate(saved):
+            subreddit = item["subreddit"]
+
+            if subreddit == "Image/External Link":
+                f.write(f"{item_ix+1}  :\n")
+            else:
+                f.write(
+                    f"{item_ix+1} : [r/{subreddit}](https://reddit.com/{subreddit})\n"
+                )
+
+            f.write(f"  -- [{item['title']}]({item['url']})\n")
+            if item["type"] == "comment":
+                f.write("\n")
+                f.write(item["content"])
+            f.write(f"\n{'*'*40}\n")
+
+
+if __name__ == "__main__":
+    main()
